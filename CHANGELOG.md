fbc6f9c35f50c3ebaca0b3497b773f92ebfa88a7
David Rauschenbach <david@megalithic.llc>
2025-05-04 16:52:18 -0700

    Remove an unused dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,4 @@
 
 ## [0.1.3] - 2025-05-04
 ### Added
-- [#11](https://github.com/saas-rs/cli/issues/1) Initial public release
+- [#1](https://github.com/saas-rs/cli/issues/1) Initial public release
--- src/kind.rs
+++ src/kind.rs
@@ -1,6 +1,4 @@
-use globwalk::DirEntry;
 use serde::Deserialize;
-use std::error::Error;
 
 #[derive(Debug, Deserialize, PartialEq)]
 pub enum Kind {
@@ -14,20 +12,4 @@ pub enum Kind {
 struct Any {
     #[allow(dead_code)]
     kind: Option<Kind>,
-}
-
-#[allow(dead_code)]
-pub fn is_kind(dir_entry: DirEntry, match_kind: Kind) -> Result<Option<DirEntry>, Box<dyn Error>> {
-    let contents = std::fs::read(dir_entry.path())?;
-    let any: Any = serde_yaml::from_slice(&contents)?;
-    Ok(match any.kind {
-        None => Some(dir_entry),
-        Some(kind) => {
-            if kind == match_kind {
-                Some(dir_entry)
-            } else {
-                None
-            }
-        }
-    })
 }
