9f4c905b708ca7de70d08a83f885bbb705fea0c6
David Rauschenbach <david@megalithic.llc>
2025-05-05 07:15:59 -0700

    Non-logging command output goes to stdout, not stderr

--- src/cmd/version.rs
+++ src/cmd/version.rs
@@ -5,6 +5,6 @@ pub struct Opts {}
 
 pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
     let version = std::env::var("VERGEN_GIT_DESCRIBE").unwrap();
-    eprintln!("{version}");
+    println!("{version}");
     Ok(())
 }
