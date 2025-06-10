d60c8a6af19203670ea26902c5d438c0f1b1c286
David Rauschenbach <david@megalithic.llc>
2025-06-10 08:58:53 -0700

    Perform git archive into an output file instead of capturing stdout in RAM

--- src/cmd/generate/do_generate_preflight.rs
+++ src/cmd/generate/do_generate_preflight.rs
@@ -3,9 +3,10 @@ use crate::protocol::saas_rs::user::v1::{upload_file_request, FileInfo, UploadFi
 use crate::{apiclient, util};
 use git2::Repository;
 use log::debug;
-use std::io::Write;
+use std::fs;
 use std::process::Command;
-use tempfile::tempfile;
+use std::str::from_utf8;
+use tempfile::NamedTempFile;
 
 pub async fn do_generate_preflight() -> Result<(String, Snapshot), Box<dyn std::error::Error>> {
     // Don't run with a bare repository
@@ -21,33 +22,39 @@ pub async fn do_generate_preflight() -> Result<(String, Snapshot), Box<dyn std::
     let project_id = util::git::find_or_create_default_project_id().await?;
 
     // Archive the workspace
+    let file = NamedTempFile::new()?;
     let output = Command::new("git")
         .arg("archive")
         .arg("--format")
         .arg("zip")
+        .arg("--output")
+        .arg(file.path().display().to_string())
         .arg("HEAD")
         .output()?;
+    if !output.stderr.is_empty() {
+        eprintln!("{}", from_utf8(&output.stderr)?);
+    }
     debug!("Archival completed");
 
     // Upload the archive
-    let mut file = tempfile()?;
-    file.write_all(&output.stdout)?;
-    let mut client = apiclient::new_user_service_client().await?;
+    let metadata = fs::metadata(file.path())?;
     let file_info = FileInfo {
-        length: output.stdout.len() as u32,
+        length: metadata.len() as u32,
         filename: "archive.zip".to_string(),
         ..Default::default()
     };
     let req1 = UploadFileRequest {
         r#type: Some(upload_file_request::Type::FileInfo(file_info)),
     };
+    let contents = fs::read(file.path())?; // TODO(https://github.com/saas-rs/cli/issues/32)
     let req2 = UploadFileRequest {
-        r#type: Some(upload_file_request::Type::Chunk(output.stdout)),
+        r#type: Some(upload_file_request::Type::Chunk(contents)),
     };
     let outbound = async_stream::stream! {
         yield req1;
         yield req2;
     };
+    let mut client = apiclient::new_user_service_client().await?;
     let file_info = client.upload_file(outbound).await?.into_inner().file_info.unwrap();
     debug!("Uploaded {file_info:?}");
 
