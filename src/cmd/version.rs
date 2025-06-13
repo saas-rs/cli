725e21c013f90ed94c81a8e0cc2a94bce56cec15
David Rauschenbach <david@megalithic.llc>
2025-06-13 07:58:38 -0700

    Fix: version subcommand panics when installed via cargo (#38)

--- src/cmd/version.rs
+++ src/cmd/version.rs
@@ -1,10 +1,15 @@
 use clap::Parser;
 
+const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+
 #[derive(Debug, Parser)]
 pub struct Opts {}
 
 pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let version = std::env::var("CARGO_PKG_VERSION").unwrap();
-    println!("{version}");
+    if let Some(version) = VERSION {
+        println!("{version}");
+    } else {
+        eprintln!("(Unknown)");
+    }
     Ok(())
 }
