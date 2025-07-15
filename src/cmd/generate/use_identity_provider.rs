2a3488c8bfc38b4bc41aea2f1959c07280c57d7a
David Rauschenbach <david@megalithic.llc>
2025-07-15 09:39:37 -0700

    Add a generator to add use of one of the supported identity providers

---
+++ src/cmd/generate/use_identity_provider.rs
@@ -0,0 +1,53 @@
+use crate::cmd::generate::{do_generate, do_generate_preflight};
+use crate::protocol::saas_rs::user::v1::{
+    generate_request::{self, use_identity_provider::Provider, UseIdentityProvider},
+    GenerateRequest,
+};
+use clap::{
+    builder::PossibleValue,
+    {Parser, ValueEnum},
+};
+
+#[derive(Debug, Parser)]
+pub struct Opts {
+    /// The identity provider
+    #[arg(long = "provider", value_enum)]
+    pub provider: Provider,
+}
+
+pub async fn run(provider: Provider) -> Result<(), Box<dyn std::error::Error>> {
+    let (project_id, snapshot) = do_generate_preflight(false).await?;
+    let req = {
+        GenerateRequest {
+            project_id,
+            snapshot: Some(snapshot),
+            what: Some(generate_request::What::UseIdentityProvider(UseIdentityProvider {
+                provider: provider as i32,
+            })),
+        }
+    };
+    do_generate(req).await
+}
+
+impl ValueEnum for Provider {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[
+            Self::Amazon,
+            Self::Apple,
+            Self::DigitalOcean,
+            Self::Facebook,
+            Self::GitHub,
+            Self::GitLab,
+            Self::Google,
+            Self::Instagram,
+            Self::Linode,
+            Self::Microsoft,
+            Self::Okta,
+            Self::Twitter,
+        ]
+    }
+
+    fn to_possible_value(&self) -> Option<PossibleValue> {
+        Some(PossibleValue::new(self.as_str_name()))
+    }
+}
