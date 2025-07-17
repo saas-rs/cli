2b96d14a3ba6100acefc6e46c833a52969a02503
David Rauschenbach <david@megalithic.llc>
2025-07-17 15:19:09 -0700

    When enabling a storage adapter, there's no need to ask for a type (#62)

--- src/cmd/generate/use_storage_adapter.rs
+++ src/cmd/generate/use_storage_adapter.rs
@@ -1,10 +1,6 @@
 use crate::cmd::generate::{do_generate, do_generate_preflight};
 use crate::protocol::saas_rs::user::v1::{
-    generate_request::{
-        self,
-        use_storage_adapter::{Provider, Type},
-        UseStorageAdapter,
-    },
+    generate_request::{self, use_storage_adapter::Provider, UseStorageAdapter},
     GenerateRequest,
 };
 use clap::{
@@ -15,15 +11,11 @@ use clap::{
 #[derive(Debug, Parser)]
 pub struct Opts {
     /// The storage provider (eg Memory, MongoDB, Redis)
-    #[arg(long = "provider", value_enum)]
+    #[arg(value_name = "provider", value_enum)]
     pub provider: Provider,
-
-    /// The storage type (one of Config, Object, Session)
-    #[arg(long = "type", value_enum)]
-    pub r#type: Type,
 }
 
-pub async fn run(provider: Provider, r#type: Type) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn run(provider: Provider) -> Result<(), Box<dyn std::error::Error>> {
     let (project_id, snapshot) = do_generate_preflight(false).await?;
     let req = {
         GenerateRequest {
@@ -31,7 +23,6 @@ pub async fn run(provider: Provider, r#type: Type) -> Result<(), Box<dyn std::er
             snapshot: Some(snapshot),
             what: Some(generate_request::What::UseStorageAdapter(UseStorageAdapter {
                 provider: provider as i32,
-                r#type: r#type as i32,
             })),
         }
     };
@@ -41,16 +32,6 @@ pub async fn run(provider: Provider, r#type: Type) -> Result<(), Box<dyn std::er
 impl ValueEnum for Provider {
     fn value_variants<'a>() -> &'a [Self] {
         &[Self::Memory, Self::MongoDb, Self::Redis]
-    }
-
-    fn to_possible_value(&self) -> Option<PossibleValue> {
-        Some(PossibleValue::new(self.as_str_name()))
-    }
-}
-
-impl ValueEnum for Type {
-    fn value_variants<'a>() -> &'a [Self] {
-        &[Self::Config, Self::Object, Self::Session]
     }
 
     fn to_possible_value(&self) -> Option<PossibleValue> {
